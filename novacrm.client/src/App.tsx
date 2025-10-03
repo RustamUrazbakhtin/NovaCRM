e6e87b93af62469b1d0ca4e7197bfa3bfc6bc7fb
Rustam Urazbakhtin <71090138+RustamUrazbakhtin@users.noreply.github.com>
2025-10-03 18:14:10 -0400

    Make dashboard widgets interactive and clickable calendar dates

--- novacrm.client/src/App.tsx
+++ novacrm.client/src/App.tsx
@@ -1,6 +1,6 @@
 ﻿import Router from "./app/router";
 import "./styles/glass.css"; // если используешь, можно оставить
-import "./styles/dashboard.css";
+import "./styles/dashboard/index.css";
 
 export default function App() {
     return <Router />;
